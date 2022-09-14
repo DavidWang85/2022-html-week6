7b1b53c0bf709e7beb45d58329bc9860c1fb23a9
王聖禮 <davidwang7777@gmail.com>
2022-09-14 23:12:48 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,53 @@
+var swiperClass = new Swiper(".mySwiper-class", {
+  spaceBetween: 16,
+  centeredSlides: true,
+  // slidesPerView: 10,
+  loop: true,
+  speed: 1500,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    },
+    992: {
+      slidesPerView: 9
+    }
+  },
+});
+var swiperFeedback = new Swiper('.mySwiper-feedback', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+var swiperProject = new Swiper(".mySwiper-project", {
+  spaceBetween: 16,
+  speed: 1500,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      autoplay: true,
+      centeredSlides: true,
+      loop: true
+    },
+    992: {
+      autoplay: false,
+      slidesPerView: 6
+    }
+  },
+});
